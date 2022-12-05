0cd8ad08be35429f71c89d1ef1c2c7fa72d671ff
Bruno Meilick <b@bnomei.com>
2022-12-05 11:19:23 +0000

    :arrow_up: upgraded dependencies, php 8
    Signed-off-by: bnomei <b@bnomei.com>

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,30 +25,11 @@ class ComposerAutoloaderInit2b360ab35e121d2510725c82bb2cddcb
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit2b360ab35e121d2510725c82bb2cddcb', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit2b360ab35e121d2510725c82bb2cddcb', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit2b360ab35e121d2510725c82bb2cddcb::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit2b360ab35e121d2510725c82bb2cddcb::getInitializer($loader));
 
         $loader->register(true);
 
